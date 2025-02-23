40c2a0ecf4a9a93b562159cf8c29bd7fdeb6b832
Arman11217 <ug2202026@cse.pstu.ac.bd>
2025-02-23 11:10:21 +0600

    name change

--- code3.java
+++ code3.java
@@ -2,6 +2,6 @@ package Test;
 
 public class code3 {
     public static void main(String[] args) {
-        System.out.println("This is java code");
+        System.out.println("My name is Arman");
     }
 }
